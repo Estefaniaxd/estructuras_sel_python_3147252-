2d2daebcc6af8275b950d5b1a4bffe0ae5e38a3d
Ana <anamariagmzz31@gmail.com>
2025-03-10 07:27:15 -1200

    operador not

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -4,6 +4,10 @@ con valores boleacos (True o False ) (V or F )
 acorde a las tablas de verdad
 
 '''
-#Ejemplo 1; Operador not:
-y= not True 
-print("el valor del resultado de operar con not es", y)
+#Ejemplo 1; Operador not:y= not True 
+print("el valor del resultado de operar con not es", y) # type: ignore
+
+
+#ejeplo 2: operador and 
+y= True and True
+print("el valor del resultado de operar con and es", y) # type: ignore
