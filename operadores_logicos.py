edd26f11c3815e03b071b67dea7f18771737b9d1
Ana <anamariagmzz31@gmail.com>
2025-03-10 10:11:51 -1200

    jerarquia

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -25,3 +25,20 @@ a = False
 b = False
 y = a or b
 print("El resultado de operar con or es: ", y) # type: ignore
+
+'''
+jerarquia de predencia de operadores 
+(logicos inclusive)
+1. ()
+2. **
+3. *, /, //, %
+4. +, -
+5. ==, !=, >, <, >=, <=
+6. not
+7. and
+8. or
+'''
+
+#ejemplo 4  jerarquia de operadores 
+y = False and not True or False
+print("El resultado de operar con jerarquia de operadores es: ", y) # type: ignore
