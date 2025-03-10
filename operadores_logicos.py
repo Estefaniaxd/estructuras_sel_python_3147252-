e8a6b1029d6a4d4b78e014af1d6df6f3b854e3ad
Ana <anamariagmzz31@gmail.com>
2025-03-10 10:23:47 -1200

    operadores relacionales y boolanos

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -41,4 +41,9 @@ jerarquia de predencia de operadores
 
 #ejemplo 4  jerarquia de operadores 
 y = False and not True or False
-print("El resultado de operar con jerarquia de operadores es: ", y) # type: ignore
+print("El resultado de operar con jerarquia de operadores es: ", y) # type: ignore 
+
+
+#ejemplos 5 : relacionales y logicos
+
+y = not 3 > 4 and 4 == 4 or 3 < 2
